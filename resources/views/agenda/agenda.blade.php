e194a16204fb020604693336aa4d2412e8fa78eb
Argaaa63 <argaaaa53@gmail.com>
2024-10-25 17:44:30 +0700

    install tailwind and npm + use template

---
+++ resources/views/agenda/agenda.blade.php
@@ -0,0 +1,11 @@
+@extends('template')
+@section('konten')
+    <div class="container flex">
+        <div class="sidebar w-2/12">
+
+        </div>
+        <div class="field w-10/12">
+
+        </div>
+    </div>
+@endsection
